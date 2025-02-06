bb8b6ebd2da33323968fc0a2669045cc00888656
Naviya4 <4navindu@gmail.com>
2025-02-06 11:18:54 +0530

    Interface with Sub class

---
+++ Console Menu System/NB Online Shopping System/src/NBShoppingManager.java
@@ -0,0 +1,22 @@
+public class NBShoppingManager implements ShoppingManager {
+    
+    @Override
+    public void addProduct() {
+
+    }
+
+    @Override
+    public void deleteProduct() {
+
+    }
+
+    @Override
+    public void printProductList() {
+
+    }
+
+    @Override
+    public void saveToFile() {
+
+    }
+}
---
+++ Console Menu System/NB Online Shopping System/src/ShoppingManager.java
@@ -0,0 +1,8 @@
+public interface ShoppingManager {
+
+    public abstract void addProduct();
+    public abstract void deleteProduct();
+    public abstract void printProductList();
+    public abstract void saveToFile();
+
+}
